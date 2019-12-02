981ba4e0bd55adef05646a13c64b50e24c608cbe
Zain Syed <zain.syed@vodafone.com>
2019-12-02 14:26:53 +0000

    download functionality fro ie

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+const download = require('downloadjs')
+const { b64 } = require('./blob.js')
+const printJS = require('print-js')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = 'data:application/pdf;base64,' + b64
+const strFileName = 'contract.pdf'
+const strMimeType = 'application/pdf'
+
+// from https://codepen.io/gapcode/pen/vEJNZN
+const isIEorEdge = () => {
+  var ua = window.navigator.userAgent
+
+  var msie = ua.indexOf('MSIE ')
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10)
+  }
+
+  var trident = ua.indexOf('Trident/')
+  if (trident > 0) {
+    // IE 11 => return version number
+    var rv = ua.indexOf('rv:')
+    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10)
+  }
+
+  var edge = ua.indexOf('Edge/')
+  if (edge > 0) {
+    // Edge (IE 12+) => return version number
+    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10)
+  }
+
+  // other browser
+  return false
 }
 
-export default App;
+// http://printjs.crabbly.com/
+const printPDF = blob => {
+  try {
+    printJS({ printable: blob, type: 'pdf', base64: true })
+  } catch (e) {
+    throw new Error('Ouch')
+  }
+}
+
+const handleDownload = () => {
+  download(data, strFileName, strMimeType)
+}
+
+function App () {
+  return (
+    <div className='App'>
+      <button onClick={handleDownload}>
+        Download {isIEorEdge() && '/ Print'}
+      </button>
+      {!isIEorEdge() && <button onClick={() => printPDF(b64)}>Print</button>}
+    </div>
+  )
+}
+
+export default App
